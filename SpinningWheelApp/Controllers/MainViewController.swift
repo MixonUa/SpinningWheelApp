98e21fd0d29928b160bb37f15a24e23ff2966174
MixonUa <mykhailofrolov@gmail.com>
2024-02-08 14:16:59 +0200

    add choice buttons

--- SpinningWheelApp/Controllers/MainViewController.swift
+++ SpinningWheelApp/Controllers/MainViewController.swift
@@ -8,9 +8,45 @@
 import UIKit
 
 class MainViewController: UIViewController {
+    
+    let viewModel = GameChoiceButtonViewModel(tittle: "Countriesesese", emodji: "😀")
+    var buttonsArray:[GameChoiceButton] = []
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        view.backgroundColor = .blue
+        
+        let firstButton = GameChoiceButton(with: viewModel)
+        buttonsArray.append(firstButton)
+        let secondButton = GameChoiceButton(with: GameChoiceButtonViewModel(tittle: "Countriesesese wowowowowo", emodji: "🦃"))
+        buttonsArray.append(secondButton)
+        configureButtons(with: buttonsArray)
+    }
+
+    private func configureButtons(with buttons: [GameChoiceButton]) {
+        firstButtonConfigure(with: buttons[0])
+        secondButtonConfigure(with: buttons[1])
+    }
+    
+    private func firstButtonConfigure(with first: GameChoiceButton) {
+        view.addSubview(first)
+        first.translatesAutoresizingMaskIntoConstraints = false
+        NSLayoutConstraint.activate([
+            first.bottomAnchor.constraint(equalTo: view.topAnchor, constant: (view.frame.height/2)-10),
+            first.leadingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.leadingAnchor, constant: 20),
+            first.widthAnchor.constraint(equalTo: view.safeAreaLayoutGuide.widthAnchor, multiplier: 0.4),
+            first.heightAnchor.constraint(equalTo: view.safeAreaLayoutGuide.widthAnchor, multiplier: 0.4)
+        ])
+    }
+    private func secondButtonConfigure(with second: GameChoiceButton) {
+        view.addSubview(second)
+        second.translatesAutoresizingMaskIntoConstraints = false
+        NSLayoutConstraint.activate([
+            second.topAnchor.constraint(equalTo: view.topAnchor, constant: (view.frame.height/2)+10),
+            second.trailingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.trailingAnchor, constant: -20),
+            second.widthAnchor.constraint(equalTo: view.safeAreaLayoutGuide.widthAnchor, multiplier: 0.4),
+            second.heightAnchor.constraint(equalTo: view.safeAreaLayoutGuide.widthAnchor, multiplier: 0.4)
+        ])
     }
 }
 
